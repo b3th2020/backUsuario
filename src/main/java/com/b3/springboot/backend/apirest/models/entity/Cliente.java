a69685dc145442d73e63761947c0a34600342f1a
B3thj <bery.sweet.23mail.com>
2021-02-13 18:22:42 -0600

    actualizacion

--- src/main/java/com/b3/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/b3/springboot/backend/apirest/models/entity/Cliente.java
@@ -34,7 +34,7 @@ public class Cliente implements Serializable{
 	private long id;
 	
 	@NotEmpty(message = "no puede estar vacio")
-	@Size(min=4, max=12, message = "el tamaño tiene que estar entre 4 y 12")
+//	@Size(min=4, max=12, message = "el tamaño tiene que estar entre 4 y 12")
 	@Column(nullable = false)
 	private String nombre;
 	
